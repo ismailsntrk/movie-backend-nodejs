c0d6066e02427d4bc380222e32b1397a771829ad
ismail Şentürk <48655332+ismailsntrk@users.noreply.github.com>
2023-05-31 13:55:18 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -192,7 +192,13 @@ exports.signin = (req, res) => {
   if (req.isAuthenticated()) {
     const { _id, name, email, role } = req.user;
     const token = signToken(_id);
-    res.cookie("access_token", token, { httpOnly: true, sameSite: true });
+         res.cookie("access_token", token, {
+       httpOnly: true,
+       secure:false,
+       domain:".https://ubiquitous-chebakia-add3da.netlify.app"
+     });
+
+   // res.cookie("access_token", token, { httpOnly: true, sameSite: true });
     res
       .status(200)
       .json({ isAuthenticated: true, user: { name, email, role } });
