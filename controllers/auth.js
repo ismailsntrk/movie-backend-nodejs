7676defb316b9fc3c53ab4c047f8474c3c910867
ismail Şentürk <48655332+ismailsntrk@users.noreply.github.com>
2023-05-31 15:24:31 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -192,7 +192,7 @@ exports.signin = (req, res) => {
   if (req.isAuthenticated()) {
     const { _id, name, email, role } = req.user;
     const token = signToken(_id);
-    res.cookie("access_token", token, { httpOnly: true, sameSite: none });
+    res.cookie("access_token", token, { httpOnly: true, sameSite: false });
     res
       .status(200)
       .json({ isAuthenticated: true, user: { name, email, role } });
